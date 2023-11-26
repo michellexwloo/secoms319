318053150306792167502341c7947676c950dfc3
xuanwen <xuanwen@iastate.edu>
2023-11-26 12:29:42 -0600

    implemented login view

---
+++ FinalProject/final_project/src/components/About.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function SubView() {
+  return (
+    <div>
+      <header>
+        <h1>Wan Yeen Tradings</h1>
+      </header>
+
+      <div id="subheader">
+        <h2>Temperature & Humidity Tracker</h2>
+      </div>
+
+      <h2>About Us</h2>
+      {/* TODO */}
+
+      <footer>
+        <p>&copy; Wan Yeen Trading's Warehouse Tracker. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+}
+
+export default SubView;
