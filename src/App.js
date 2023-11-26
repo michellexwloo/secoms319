79f0a7a82cf3180b2b3edc2813e7bca8265f3910
xuanwen <xuanwen@iastate.edu>
2023-11-26 11:50:04 -0600

    Created final project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Login from './components/Login';
+import MainView from './components/MainView';
+import SubView from './components/SubView';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [loggedIn, setLoggedIn] = useState(false);
+
+    const handleLogin = () => {
+        setLoggedIn(true);
+    };
+
+    return (
+        <div>
+            {!loggedIn ? (
+                <Login onLogin={handleLogin} />
+            ) : (
+                <MainView />
+            )}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/MainView.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function MainView() {
+  return (
+    <div>
+      <header>
+        <h1>Wan Yeen Tradings</h1>
+      </header>
+
+      <div id="subheader">
+        <h2>Temperature & Humidity Tracker</h2>
+      </div>
+
+      <h1>Main View</h1>
+      {/* TODO */}
+
+      <footer>
+        <p>&copy; Wan Yeen Trading's Warehouse Tracker. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+}
+
+export default MainView;
