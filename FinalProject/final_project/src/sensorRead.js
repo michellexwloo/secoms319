9b8cd9a6d41f6cd71b9b258f8b83d8da518a3208
Chris Smith <christopherlsmith@live.com>
2023-11-26 20:22:38 -0600

    update

--- FinalProject/final_project/src/sensorRead.js
+++ FinalProject/final_project/src/sensorRead.js
@@ -1,12 +1,12 @@
 
-const sensor = require('node-dht-sensor');
+import { read } from 'node-dht-sensor';
 
 const sensorType = 11;
 const pin =7;
 let temperatureStore =0;
 let humidityStore =0;
 
-sensor.read(sensorType, pin, (temperature, humidity)=>{
+read(sensorType, pin, (temperature, humidity)=>{
 
         temperatureStore =temperature;
         humidityStore =humidity;
@@ -15,16 +15,16 @@ sensor.read(sensorType, pin, (temperature, humidity)=>{
 });
 
 
-const http = require("http");
-const server = http.createServer((req, res) => {
+import { createServer } from "http";
+const server = createServer((req, res) => {
         console.log(req.url);
         res.writeHead(200, { "Content-Type": "text/plain" });
         res.end("READING TEMPERATURES");
         while(1){
-                res.end(`Temperature: ${temperature.toFixed(1)}°C, Humidity: ${humidity.toFixed(1)}%`);
+                res.end(`Temperature: ${temperatureStore.toFixed(1)}°C, Humidity: ${humidityStore.toFixed(1)}%`);
         }
 });
 console.log("Server is listening on port 3000");
-server.listen(3000, "127.0.0.1"); // ssh pi@10.26.27.96
+server.listen(3000, "10.26.27.96"); // ssh pi@10.26.27.96
 
 
